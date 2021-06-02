5bfe8824c901cb707e8ef1ea856783cd7fdee551
shvashakidze <shvashakidze@users.noreply.github.com>
2021-06-02 15:11:28 -0700

    reset part2-8

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,10 +1,8 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
 
-const getAll2 = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
 
 const getAll = () => {
   const request = axios.get(baseUrl)
